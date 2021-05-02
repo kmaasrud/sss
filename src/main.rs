23cfc7a0accff0bf416b21e323d7d7b401e8e272
kmaasrud <km@aasrud.com>
2021-05-02 20:36:00 +0200

    Basic parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod slide;
+
+use std::fs::File;
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("test").unwrap();
+    for line in io::BufReader::new(file).lines() {
+        if let Ok(s) = line {
+            if s.trim().is_empty() {
+                println!("Is empty line");
+            }
+        }
+    }
 }
---
+++ src/slide.rs
@@ -0,0 +1,3 @@
+struct _Slide {
+    text: String,
+}
