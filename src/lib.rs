18c4ca72d7d4e6a6abaf287f9877c90cde769cf1
kmaasrud <km@aasrud.com>
2022-09-16 07:57:43 +0200

    feat: add util funcs for loading directories

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,15 @@ pub use template::{preprocess, Template};
 
 mod walk;
 pub use walk::RecursiveWalker;
+
+pub fn src_dir() -> std::ffi::OsString {
+    std::env::var_os("SSS_SRC").unwrap_or_else(|| String::from("src").into())
+}
+
+pub fn dst_dir() -> std::ffi::OsString {
+    std::env::var_os("SSS_DST").unwrap_or_else(|| String::from("dst").into())
+}
+
+pub fn tmpl_dir() -> std::ffi::OsString {
+    std::env::var_os("SSS_TMPL").unwrap_or_else(|| String::from("tmpl").into())
+}
