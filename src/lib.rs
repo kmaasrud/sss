770540266d37b2dfd44aca83342edb70f9c71c86
kmaasrud <km@aasrud.com>
2022-09-18 12:05:23 +0200

    refactor: use macro instead of multiple funcs

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,9 @@ pub use template::{preprocess, Template};
 mod walk;
 pub use walk::RecursiveWalker;
 
-pub fn src_dir() -> std::ffi::OsString {
-    std::env::var_os("SSS_SRC").unwrap_or_else(|| String::from("src").into())
-}
-
-pub fn dst_dir() -> std::ffi::OsString {
-    std::env::var_os("SSS_DST").unwrap_or_else(|| String::from("dst").into())
-}
-
-pub fn tmpl_dir() -> std::ffi::OsString {
-    std::env::var_os("SSS_TMPL").unwrap_or_else(|| String::from("tmpl").into())
+#[macro_export]
+macro_rules! env_with_default {
+    ($env:expr, $default:expr) => {
+        std::env::var_os($env).unwrap_or_else(|| $default.into())
+    };
 }
